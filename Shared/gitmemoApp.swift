4920419fdd0aeec54f492025ec3480efdfd8206e
David <davidgalet@gmail.com>
2021-01-12 15:54:52 +0100

    Initial Commit

---
+++ Shared/gitmemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitmemoApp.swift
+//  Shared
+//
+//  Created by David Galet on 12/01/21.
+//
+
+import SwiftUI
+
+@main
+struct gitmemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
